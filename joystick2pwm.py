196c0b6dd1b57788544a07455fd104362e338064
tmcnugget <graycday@icloud.com>
2025-01-12 08:58:36 +0000

    Create joystick2pwm.py

---
+++ joystick2pwm.py
@@ -0,0 +1,28 @@
+import sys
+
+def main():
+    if len(sys.argv) != 4:
+        print("Usage: motor.py <axis_0> <axis_1> <axis_2>")
+        return
+
+    # Read axis data from command-line arguments
+    lrRAW = float(sys.argv[1])
+    fbRAW = float(sys.argv[2])
+    rRAW = float(sys.argv[3])
+
+    speed = 1
+
+    if len(sys.argv) > 5:
+        speed += float(sys.argv[4])/10 - float(sys.argv[5])/10
+
+    lr = round(lrRAW, 3)/2 * speed
+    fb = round(fbRAW, 3)/2 * speed
+    r = round(rRAW, 3)/2 * speed
+
+    m1 = fb + lr + r
+    m2 = fb - lr - r
+    m3 = fb - lr + r
+    m4 = fb + lr - r
+
+if __name__ == "__main__":
+    main()
