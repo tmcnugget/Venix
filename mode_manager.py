247c3108c9cc010b4301e5c3f04bba116fd2d7c4
tmcnugget <graycday@icloud.com>
2025-01-13 15:40:18 +0000

    Update mode_manager.py

--- mode_manager.py
+++ mode_manager.py
@@ -1,4 +1,19 @@
 import sys
+import subprocess
 
 select = int(sys.argv[1])
 funtion = int(sys.argv[2])
+
+mode = 0
+modecount = 2 # Excluding passive
+
+if select == 1:
+    if 0 <= mode < modecount:
+        if r == 1:
+            mode += 1
+    if 0 < mode <= modecount:
+        if l == 1:
+            mode -= 1
+
+while True:
+    subprocess.run(["python3", "Venix/oled.py", str(mode)])
