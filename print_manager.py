712700ad93a2bd524612ae0b6c69c3551f60b841
tmcnugget <graycday@icloud.com>
2025-01-14 07:12:24 +0000

    Create print_manager.py

---
+++ print_manager.py
@@ -0,0 +1,13 @@
+import sys
+
+lr = float(sys.argv[1])
+fb = float(sys.argv[2])
+r = float(sys.argv[3])
+zl = int(sys.argv[4])
+zr = int(sys.argv[5])
+s = int(sys.argv[6])
+f = int(sys.argv[7])
+l = int(sys.argv[8])
+r = int(sys.argv[9])
+
+print(lr, fb, r, zl, zr, s, f, l, r)
