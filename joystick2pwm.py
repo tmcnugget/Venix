bb1c3653cd787569ce5ac82f48333837c9914878
tmcnugget <graycday@icloud.com>
2025-01-15 06:36:59 +0000

    Update joystick2pwm.py

--- joystick2pwm.py
+++ joystick2pwm.py
@@ -6,28 +6,19 @@ def main():
         print("Usage: motor.py <axis_0> <axis_1> <axis_2> <zl> <zr>")
         return
 
-    # Read axis data from command-line arguments
-    lrRAW = float(sys.argv[1])
-    fbRAW = float(sys.argv[2])
-    rRAW = float(sys.argv[3])
-
     speed = 1
+    speed = max(0, min(speed + float(sys.argv[4])/10 - float(sys.argv[5])/10, 2))
 
-    if len(sys.argv) > 5:
-        speed += float(sys.argv[4])/10 - float(sys.argv[5])/10
-
-    speed = max(0, min(speed, 2))
-
-    lr = round(lrRAW, 3)/2 * speed
-    fb = round(fbRAW, 3)/2 * speed
-    r = round(rRAW, 3)/2 * speed
+    lr = float(sys.argv[1])/2 * speed
+    fb = float(sys.argv[2])/2 * speed
+    r = float(sys.argv[3])/2 * speed
 
     m1 = fb + lr + r
     m2 = fb - lr - r
     m3 = fb - lr + r
     m4 = fb + lr - r
 
-    subprocess.run(["python3", "Venix/pca9685.py", str(m1), str(m2), str(m3), str(m4)])
+    subprocess.Popen(["python3", "Venix/pca9685.py", str(m1), str(m2), str(m3), str(m4)])
 
 if __name__ == "__main__":
     main()
