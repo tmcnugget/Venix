8b11d997d5e26e186dabe13f2f0f4a14943a596b
tmcnugget <graycday@icloud.com>
2025-01-26 12:28:09 +0000

    Update oled.py

--- oled.py
+++ oled.py
@@ -1,3 +1,4 @@
+import sys
 from luma.oled.device import ssd1306
 from luma.core.interface.serial import i2c
 from luma.core.render import canvas
@@ -8,21 +9,37 @@ def text(text, size, x, y):
         font = ImageFont.truetype("font.ttf", size)
         draw.text((x, y), text, font = font, fill="white")
 
-def initOLED():
-    # Create I2C interface
+function = sys.argv[1]
+
+def getValues():
+    lr = float(sys.argv[1])
+    fb = float(sys.argv[2])
+    r = float(sys.argv[3])
+    return lr, fb, r
+
+def init():
     serial = i2c(port=1, address=0x3C)
-    # Create an SSD1306 OLED device
     device = ssd1306(serial)
-
-def showOLED():
-    drawmain()
-        
-def writeOLED(lr, fb, r, m1, m2, m3, m4):
-    drawmain()
     
+    text("Joystick values:", 25, 5, 0)
+    text("left-right:", 15, 5, 25)
+    text("fwd-bckwd:", 15, 5, 40)
+    text("rotate", 15, 5, 55)
+    text("E O", 15, 25, 25)
+    text("R R", 15, 25, 40)
+    text("R !", 15, 25, 55)
 
-def drawmain():
-    text(m1, 10, 5, 0)
-    text(m2, 10, 15, 0)
-    text(m3, 10, 25, 0)
-    text(m4, 10, 35, 0)
+def write():
+    text("Joystick values:", 25, 5, 0)
+    text("left-right:", 15, 5, 25)
+    text("fwd-bckwd:", 15, 5, 40)
+    text("rotate", 15, 5, 55)
+    text(f"{lr:.2f}", 15, 5, 50)
+    text(f"{fb:.2f}", 15, 5, 40)
+    text(f"{r:.2f}", 15, 5, 55)
+
+if function == "init":
+    init()
+elif function == "write":
+    getValues()
+    write()
