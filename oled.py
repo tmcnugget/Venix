f7ab1c8af08fc047b3fff9d13bb8c44dcd848604
tmcnugget <graycday@icloud.com>
2025-01-26 10:54:17 +0000

    Update oled.py

--- oled.py
+++ oled.py
@@ -3,17 +3,24 @@ from luma.core.interface.serial import i2c
 from luma.core.render import canvas
 from PIL import ImageFont
 
-# Create I2C interface
-serial = i2c(port=1, address=0x3C)
+def initOLED()
+    # Create I2C interface
+    serial = i2c(port=1, address=0x3C)
+    # Create an SSD1306 OLED device
+    device = ssd1306(serial)
 
-# Create an SSD1306 OLED device
-device = ssd1306(serial)
+def showOLED()
+    drawmain()
+        
+def writeOLED(lr, fb, r, m1, m2, m3, m4)
+    drawmain()
+    
 
-while True:
+def drawmain()
     with canvas(device) as draw:
-        # Load custom fonts
-        large_font = ImageFont.truetype("Venix/font.ttf", 35)
-        small_font = ImageFont.truetype("Venix/font.ttf", 15)
+            # Load custom fonts
+            large_font = ImageFont.truetype("font.ttf", 35)
+            small_font = ImageFont.truetype("font.ttf", 15)
 
-        # Draw text on the OLED
-        draw.text((5, 0), "Venix", font=large_font, fill="white")
+            # Draw text on the OLED
+            draw.text((5, 0), "Venix", font=large_font, fill="white")
