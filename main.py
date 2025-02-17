05bb3cc4c007c5eef32c8a5aaa63b6892648b876
tmcnugget <graycday@icloud.com>
2025-02-17 06:51:53 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ import os
 import pygame
 import subprocess
 import time
+import math
 from adafruit_pca9685 import PCA9685
+from icm20948 import ICM20948
 import board
 import busio
 
@@ -17,6 +19,8 @@ pygame.joystick.init()
 i2c = busio.I2C(board.SCL, board.SDA)
 pca = PCA9685(i2c)
 pca.frequency = 60  # Set the PWM frequency
+
+imu = ICM20948()
 
 # A dictionary to keep track of connected joysticks
 joysticks = {}
@@ -82,10 +86,73 @@ def setMotors(lr, fb, r):
 
     print(m1, m2, m3, m4)
 
+def heading():
+    global heading
+    
+    # Read the current, uncalibrated, X, Y & Z magnetic values from the magnetometer and save as a list
+    mag = list(imu.read_magnetometer_data())
+
+    # Step through each uncalibrated X, Y & Z magnetic value and calibrate them the best we can
+    for i in range(3):
+        v = mag[i]
+        
+        # If our current reading (mag) is less than our stored minimum reading (amin), then save a new minimum reading
+        # ie save a new lowest possible value for our calibration of this axis
+        if v < amin[i]:
+            amin[i] = v
+            
+        # If our current reading (mag) is greater than our stored maximum reading (amax), then save a new maximum reading
+        # ie save a new highest possible value for our calibration of this axis
+        if v > amax[i]:
+            amax[i] = v
+
+        # Calibrate value by removing any offset when compared to the lowest reading seen for this axes
+        mag[i] -= amin[i]
+
+        # Scale value based on the highest range of values seen for this axes
+        # Creates a calibrated value between 0 and 1 representing magnetic value
+        try:
+            mag[i] /= amax[i] - amin[i]
+        except ZeroDivisionError:
+            pass
+        # Shift magnetic values to between -0.5 and 0.5 to enable the trig to work
+        mag[i] -= 0.5
+
+    # Convert from Gauss values in the appropriate 2 axis to a heading in Radians using trig
+    # Note this does not compensate for tilt
+    heading = math.atan2(
+            mag[AXES[0]],
+            mag[AXES[1]])
+
+    # If heading is negative, convert to positive, 2 x pi is a full circle in Radians
+    if heading < 0:
+        heading += 2 * math.pi
+
+    # Convert heading from Radians to Degrees
+    heading = math.degrees(heading)
+    # Round heading to nearest full degree
+    heading = round(heading)
+
+def calibrate:
+    global AXES = Y, Z
+
+    amin = list(imu.read_magnetometer_data())
+    amax = list(imu.read_magnetometer_data())
+
+    setMotors(0, 0, 0.5)
+    time.sleep(5)
+    setMotors(0, 0, 0)
+
 def main():
     speed = 1
     zl = 0
     zr = 0
+
+    global X = 0
+    global Y = 1
+    global Z = 2
+
+    heading()
     
     print("Starting headless joystick controller...")
 
@@ -116,6 +183,11 @@ def main():
 
                 zl = joystick.get_button(6)
                 zr = joystick.get_button(7)
+                
+            ax, ay, az, gx, gy, gz = imu.read_accelerometer_gyro_data()
+    
+            temperature = icm20948.read_temperature()
+            round(temperate, 2)
 
             """Adjusts the speed based on joystick button inputs."""
             if zr == 1:
@@ -133,4 +205,5 @@ def main():
         pygame.quit()
 
 if __name__ == "__main__":
+    calibrate()
     main()
